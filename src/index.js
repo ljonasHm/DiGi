0c8abc17b74a7510c30a2af523a4bef5cd9439b5
ljonasHm <shamordinanton@gmail.com>
2021-05-13 11:25:04 +0300

    configurated slider for 640px width

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ $(function(){
         dots: true,
         slidesToShow: 1,
         slidesToScroll: 1,
+        nextArrow: '<button type="button" class="slick-arrow slick-next"><img src="images/next.svg" alt="next-arrow"></button>',
+        prevArrow: '<button type="button" class="slick-arrow slick-prev"><img src="images/prev.svg" alt="prev-arrow"></button>',
         responsive: [
             {
                 breakpoint: 1024,
